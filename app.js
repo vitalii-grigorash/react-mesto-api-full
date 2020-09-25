92b4ca66ee837d1f03587911ae537c806e6858fa
Vitalii Grigorash <vitalii.grigorash@yandex.ru>
2020-09-25 17:23:28 +0300

    added crash-test

--- app.js
+++ app.js
@@ -24,6 +24,12 @@ mongoose.connect("mongodb://localhost:27017/mestodb", {
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post("/signin", loginValidation, login);
 app.post("/signup", userValidation, createUser);
 
